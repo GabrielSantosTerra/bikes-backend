a55a0dfa5e0ee6bfaa904fa67f205de4e7099296
Gabriel Terra <gabrielsantosterra@gmail.com>
2025-02-28 17:15:30 -0300

    Python certo

--- src/app/utils/hashPassword.py
+++
@@ -1,8 +0,0 @@
-from passlib.context import CryptContext
-
-# Configuração do bcrypt para hash de senha
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-# Função para gerar o hash da senha
-def hash_password(password: str) -> str:
-    return pwd_context.hash(password)
