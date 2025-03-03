33021a659f8f1d837fc98115fb41230e45457de9
Gabriel Terra <gabrielsantosterra@gmail.com>
2025-03-03 14:45:19 -0300

    correção na estrutura do back

--- db_init.py
+++
@@ -1,6 +0,0 @@
-from app.config.database import engine
-from app.models.userModel import Base
-
-# Criar todas as tabelas definidas nos modelos
-Base.metadata.create_all(bind=engine)
-print("Tabelas criadas com sucesso!")
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import uvicorn
-from app.main import app
+from main import app
 from dotenv import load_dotenv
 import os
 
